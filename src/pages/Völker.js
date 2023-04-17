8c5441adcdf5994c84cd95acfca03368352ede27
AnnScherer <ann.hardt@gmx.de>
2023-04-17 15:56:46 +0200

    alle völker

--- src/pages/Völker.js
+++ src/pages/Völker.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useContext } from "react";
+import userContext from "../context/userContext";
 
 export default function Völker() {
+  const { volk } = useContext(userContext);
+  console.log(volk);
+
   return (
-    <div>Völker</div>
-  )
+    <div className="völker">
+      {volk.map((info) => {
+        return (
+          <div key={info._id} className="volk">
+            <h3>{info.name}</h3>
+            {info.text.map((text, i) => (
+              <p dangerouslySetInnerHTML={{ __html: text }} key={i}></p>
+            ))}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
