da30a147ff7b10bd89b8a8d70371a08283f5ac7a
Ramon <ramon.mitra81@gmail.com>
2023-04-17 11:18:33 +0200

    dicer hinzugefuegt

---
+++ src/components/dicer/wThree.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import style from "./DicerStyle.module.css";
+
+const DicerPoolWThree = () => {
+  const [items, setItems] = useState([]);
+
+  const addWThree = () => {
+    setItems((cur) => [
+      ...cur,
+      { id: cur.length, number: Math.floor(Math.random() * 3 + 1) },
+    ]);
+  };
+
+  return (
+    <div>
+      <button className={style.button2} onClick={() => setItems([])}>
+        Remove
+      </button>
+      <button className={style.button} onClick={addWThree}>
+        Add <br /> W3
+        {items.map((item) => {
+          return (
+            <div key={item.id}>
+              <div className={style.cube}>
+                <div className={style.front}>
+                  <a className={style.img}></a>
+                </div>
+                <div className={style.top}>
+                  <a className={style.a}>{item.number}</a>
+                </div>
+                <div className={style.left}>
+                  <a className={style.img}></a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </button>
+    </div>
+  );
+};
+
+export default DicerPoolWThree;
