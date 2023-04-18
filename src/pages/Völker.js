1f3a15db9715fbec9e784a59bde76a333f8e52a1
AnnScherer <ann.hardt@gmx.de>
2023-04-18 13:14:01 +0200

    völker

--- src/pages/Völker.js
+++ src/pages/Völker.js
@@ -2,21 +2,58 @@ import React, { useContext } from "react";
 import userContext from "../context/userContext";
 
 export default function Völker() {
-  const { volk } = useContext(userContext);
-  console.log(volk);
+  const { volk, filterVolk, setFilterVolk } = useContext(userContext);
+
+  const onChangeVolk = (event) => {
+    const value = event.target.value;
+    const volkFilter = volk.filter((info) => {
+      return info.name === value;
+    });
+    setFilterVolk(volkFilter);
+  };
+  console.log(filterVolk);
 
   return (
     <div className="völker">
-      {volk.map((info) => {
-        return (
-          <div key={info._id} className="volk">
-            <h3>{info.name}</h3>
-            {info.text.map((text, i) => (
-              <p dangerouslySetInnerHTML={{ __html: text }} key={i}></p>
-            ))}
-          </div>
-        );
-      })}
+      <div className="select">
+        <div className="select-volk">
+          <label>Volk auswählen:</label>
+          <select onChange={onChangeVolk}>
+            <option>Volk auswählen</option>
+            <option>ELFEN</option>
+            <option>DRACHENBLÜTIGE</option>
+            <option>GNOME</option>
+            <option>HALBELFEN</option>
+            <option>HALBLINGE</option>
+            <option>MENSCHEN</option>
+            <option>TIEFLINGE</option>
+            <option>ZWERGE</option>
+          </select>
+        </div>
+      </div>
+      {filterVolk.length > 0 ? (
+        <>
+          {filterVolk.map((volk, i) => {
+            return (
+              <div className="volk" key={i}>
+                <h3 key={i}>{volk.name}</h3>
+                {volk.text.map((info, i) => {
+                  return (
+                    <p dangerouslySetInnerHTML={{ __html: info }} key={i}></p>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <div className="volk">
+          <h3>{volk[2].name}</h3>
+          {volk[2].text.map((info, i) => (
+            <p dangerouslySetInnerHTML={{ __html: info }} key={i}></p>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
