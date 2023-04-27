561b8ec79babf7654feb3390f4005c2ea4438faf
Eduard Metzler <eduard.metzler@mail.ru>
2023-04-27 13:10:58 +0200

    bug fix

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -42,17 +42,18 @@ export default function Dashboard() {
                         <h2>Passwort änderung</h2>
                         <form onSubmit={handleSubmit}>
                             <input
-                                type="text"
+                                className=""
+                                type="password"
                                 name="password"
-                                placeholder="Passwort"
+                                placeholder="Dein Passwort"
                                 onChange={(e) =>
                                     setPasswordChange({ ...passwordChange, password: e.target.value })
                                 }
                             />
-                            <input
+                            <input className=""
                                 type="password"
                                 name="newPasswort"
-                                placeholder="neue Passwort"
+                                placeholder="Neue Passwort"
                                 onChange={(e) =>
                                     setPasswordChange({ ...passwordChange, newPassword: e.target.value })
                                 }
