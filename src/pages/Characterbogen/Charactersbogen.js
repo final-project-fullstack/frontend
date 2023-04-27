d27069c1954a19c0320cf2a30a922a6aaf30b59e
Ramon <ramon.mitra81@gmail.com>
2023-04-27 12:26:20 +0200

    characterbogen style geändert

--- src/pages/Characterbogen/Charactersbogen.js
+++ src/pages/Characterbogen/Charactersbogen.js
@@ -6,8 +6,10 @@ const CharSheet = () => {
       <div class={style.kopfzeile}>
         {/* <div class={style.charName}>Character Name</div> */}
         <div class={style.nameContainer}>
-          <textarea rows="2" name="charName"></textarea>
-          <div class={style.labelName}>Character Name</div>
+          <textarea rows="2" name="charName" id="charName"></textarea>
+          <label for="charName" class={style.labelName}>
+            Character Name
+          </label>
         </div>
         <div class={style.charInfo}>
           <p class={style.pInfo}>
@@ -61,6 +63,8 @@ const CharSheet = () => {
           <div class={style.attributeBoni}>boni</div>
         </div>
       </div>
+
+      {/* ANCHOR Inspiration & Übung */}
       <div class={style.inspirationUndÜbung}>
         <div class={`${style.inspiration}`}>
           <div class={style.übungswert}>Wert</div>
@@ -71,6 +75,8 @@ const CharSheet = () => {
           <div>Übungsbonus</div>
         </div>
       </div>
+
+      {/*  */}
       <div class={style.div4}>4 </div>
       <div class={style.div5}>5 </div>
 
@@ -79,7 +85,6 @@ const CharSheet = () => {
       {/* ANCHOR Rettungswürfe */}
       <div class={style.rettungswürfeContainer}>
         <div class={style.rettungswurf2}>Rettungswürfe</div>
-        <br />
         {/*  */}
         <div class={style.rettungswurf}>
           <input
@@ -90,7 +95,9 @@ const CharSheet = () => {
             id="checkstaerke"
           ></input>
           <div>input</div>
-          <label for="checkstaerke">stärke</label>
+          <label class={style.label} for="checkstaerke">
+            stärke
+          </label>
         </div>
         {/*  */}
         <div class={style.rettungswurf}>
@@ -102,7 +109,9 @@ const CharSheet = () => {
             id="checkcharisma"
           ></input>
           <div>input</div>
-          <label for="checkcharisma">Charisma</label>
+          <label class={style.label} for="checkcharisma">
+            Charisma
+          </label>
         </div>
         {/*  */}
         <div class={style.rettungswurf}>
@@ -114,7 +123,9 @@ const CharSheet = () => {
             id="checkkonstitution"
           ></input>
           <div>input</div>
-          <label for="checkkonstitution">Konstitution</label>
+          <label class={style.label} for="checkkonstitution">
+            Konstitution
+          </label>
         </div>
         {/*  */}
         <div class={style.rettungswurf}>
@@ -126,7 +137,9 @@ const CharSheet = () => {
             id="checkintelligenz"
           ></input>
           <div>input</div>
-          <label for="checkintelligenz">Intelligenz</label>
+          <label class={style.label} for="checkintelligenz">
+            Intelligenz
+          </label>
         </div>
         {/*  */}
         <div class={style.rettungswurf}>
@@ -138,22 +151,300 @@ const CharSheet = () => {
             id="checkweisheit"
           ></input>
           <div>input</div>
-          <label for="checkweisheit">Weisheit</label>
+          <label class={style.label} for="checkweisheit">
+            Weisheit
+          </label>
         </div>
         {/*  */}
         <div class={style.rettungswurf}>
           <input
+            class={style.input}
             type="checkbox"
             name="checkcharisma"
             value="true"
             id="checkcharisma"
           ></input>
           <div>input</div>
-          <label for="checkcharisma">Charisma</label>
+          <label class={style.label} for="checkcharisma">
+            Charisma
+          </label>
         </div>
       </div>
-      {/* ________________________________________ */}
-      <div class={style.div7}>7</div>
+
+      {/* ANCHOR Fertigkeiten*/}
+
+      <div class={style.fertigkeitenContainer}>
+        Fertigkeiten
+        {/* ------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkAkrobatik"
+            value="true"
+            id="checkAkrobatik"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkAkrobatik">
+            Akrobatik (Ges)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkArkaneKunde"
+            value="true"
+            id="checkArkaneKunde"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkArkaneKunde">
+            Arkane Kunde (Int)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkAthletik"
+            value="true"
+            id="checkAthletik"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkAthletik">
+            Athletik (Sta)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkAuftreten"
+            value="true"
+            id="checkAuftreten"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkAuftreten">
+            Auftreten (Cha)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkEinschuechtern"
+            value="true"
+            id="checkEinschuechtern"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkEinschuechtern">
+            Einschuechtern (Cha)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkFingerfertigkeit"
+            value="true"
+            id="checkFingerfertigkeit"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkFingerfertigkeit">
+            Fingerfertigkeit (Ges)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkGeschichte"
+            value="true"
+            id="checkGeschichte"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkGeschichte">
+            Geschichte (Int)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkHeilkunde"
+            value="true"
+            id="checkHeilkunde"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkHeilkunde">
+            Heilkunde (Wei){" "}
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkHeimlichkeit"
+            value="true"
+            id="checkHeimlichkeit"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkHeimlichkeit">
+            Heimlichkeit (Ges)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkTierUmgang"
+            value="true"
+            id="checkTierUmgang"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkTierUmgang">
+            Mit Tieren umgehen (Wei)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkTierUmgang"
+            value="true"
+            id="checkTierUmgang"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkTierUmgang">
+            Mit Tieren umgehen (Wei)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkMotiv"
+            value="true"
+            id="checkMotiv"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkMotiv">
+            Motiv erkennen (Wei)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkNachforschungen"
+            value="true"
+            id="checkNachforschungen"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkNachforschungen">
+            Nachforschungen (Int)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkNaturkunde  "
+            value="true"
+            id="checkNaturkunde"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkNaturkunde">
+            Naturkunde (Int)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkReligion"
+            value="true"
+            id="checkReligion"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkReligion">
+            Religion (Int)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkTauschen"
+            value="true"
+            id="checkTauschen"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkTauschen">
+            Tauschen (Cha)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkUeberlebenskunst"
+            value="true"
+            id="checkUeberlebenskunst"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkUeberlebenskunst">
+            Überlebenskunst (Wei)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkUeberzeugen"
+            value="true"
+            id="checkUeberzeugen"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkUeberzeugen">
+            Überzeugen (Cha)
+          </label>
+        </div>
+        {/* ------------------ */}
+        <div class={style.fertigkeit}>
+          <input
+            class={style.input}
+            type="checkbox"
+            name="checkWahrnehmung"
+            value="true"
+            id="checkWahrnehmung"
+          ></input>
+          <div>input</div>
+          <label class={style.label} for="checkWahrnehmung">
+            Wahrnehmung (Wei)
+          </label>
+        </div>
+        {/* ------------------ */}
+      </div>
+
+      {/* ----------------------------------- */}
       <div class={style.div8}>8</div>
       <div class={style.div9}>9</div>
       <div class={style.div10}>10 </div>
