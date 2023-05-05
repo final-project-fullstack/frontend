439958fbbf9325d23485ff4b33d30820d13e2d30
exestiert <steffenlw94@gmail.com>
2023-05-05 10:57:51 +0200

    class zu Classname geändert bei FooterIcon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,36 +13,36 @@ const Footer = () => {
             <div className={style.card}>
                 <p>Ann</p>
                 <NavLink to="https://github.com/AnnScherer">
-                    <i class="fa-brands fa-github fa-lg"></i>
+                    <i className="fa-brands fa-github fa-lg"></i>
 
 
                 </NavLink>
                 <NavLink to="https://www.linkedin.com/in/anyamani-scherer-160538269/">
-                    <i class="fa-brands fa-linkedin"></i>
+                    <i className="fa-brands fa-linkedin"></i>
 
                 </NavLink>
             </div>
             <div className={style.card}>
                 <p>Eduard</p>
                 <NavLink to="https://github.com/EduardMetzler">
-                    <i class="fa-brands fa-github fa-lg"></i>
+                    <i className="fa-brands fa-github fa-lg"></i>
 
 
                 </NavLink>
                 <NavLink to="https://www.linkedin.com/in/eduard-metzler-092a84216/">
-                    <i class="fa-brands fa-linkedin"></i>
+                    <i className="fa-brands fa-linkedin"></i>
 
                 </NavLink>
             </div>
             <div className={style.card}>
                 <p>Steffen</p>
                 <NavLink to="https://github.com/exestiert">
-                    <i class="fa-brands fa-github fa-lg"></i>
+                    <i className="fa-brands fa-github fa-lg"></i>
 
 
                 </NavLink>
                 <NavLink to="https://www.linkedin.com/in/steffen-lemke-b27825224/">
-                    <i class="fa-brands fa-linkedin"></i>
+                    <i className="fa-brands fa-linkedin"></i>
 
                 </NavLink>
             </div>
@@ -50,12 +50,12 @@ const Footer = () => {
             <div className={style.card}>
                 <p>Ramon</p>
                 <NavLink to="4">
-                    <i class="fa-brands fa-github fa-lg"></i>
+                    <i className="fa-brands fa-github fa-lg"></i>
 
 
                 </NavLink>
                 <NavLink to="4">
-                    <i class="fa-brands fa-linkedin"></i>
+                    <i className="fa-brands fa-linkedin"></i>
 
                 </NavLink>
             </div>
