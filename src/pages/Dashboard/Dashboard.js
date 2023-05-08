1157799c1c654158a81f978f34300987f563e924
exestiert <steffenlw94@gmail.com>
2023-05-08 14:05:17 +0200

    Logo Datei gewechselt und Upload feld hinzugefügt

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useStore } from "../../context/storeContext.js";
 import axios from "axios";
 import style from "./dashboard.module.css";
 
 export default function Dashboard() {
-  const { isLoggedIn, user, setIsLoggedIn, setUser } = useStore();
+  const { isLoggedIn, user, profileImage, setProfileImage } = useStore();
   const [passwordChange, setPasswordChange] = useState({
     password: "",
     newPassword: "",
@@ -25,7 +25,38 @@ export default function Dashboard() {
       console.log(error);
     }
   };
+  const handleSubmit2 = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/uploadImage",
+        profileImage,
+        { withCredentials: true }
+      );
+      setMessage(response.data.msg);
+    } catch (error) {
+      setMessage(error.message);
+      console.log(error);
+    }
+  };
+  
 
+  function setImage(evt) {
+
+    const file = evt.target.files[0];
+
+    const fileReader = new FileReader();
+
+    fileReader.readAsDataURL(file)
+
+    fileReader.onloadend = (evt) => {
+
+        const fileData = fileReader.result;
+
+        //Je nach Input (evt.target) wird Front- oder Backimage gesetet
+        setProfileImage({img: fileData});
+    }
+};
   return (
     <div>
       {isLoggedIn && (
@@ -36,6 +67,8 @@ export default function Dashboard() {
           <div className={style.passwordChange}>
             <h2>Passwort ändern</h2>
             <form className={style.form} onSubmit={handleSubmit}>
+              <input type="file" onChange={setImage}/>
+              <button onClick={handleSubmit2}>Upload</button>
               <input
                 className={style.input}
                 type="password"
