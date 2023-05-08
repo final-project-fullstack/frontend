31ac4ec9ef0bed6332a051b5a1b44771f0c4b0d4
AnnScherer <ann.hardt@gmx.de>
2023-05-08 09:27:27 +0200

    checkbox speichern

--- src/pages/Abenteuerausrüstung/Abenteuerausrüstung.js
+++ src/pages/Abenteuerausrüstung/Abenteuerausrüstung.js
@@ -36,7 +36,7 @@ export default function Abenteuerausrüstung() {
             {filterAusrüstung[0].array.map((item, i) => {
               return (
                 <div key={item._id}>
-                  <h3>{item.name}</h3>
+                  <h3 className={style.h3}>{item.name}</h3>
 
                   {item.hasOwnProperty("kosten") && (
                     <p>
@@ -59,13 +59,19 @@ export default function Abenteuerausrüstung() {
                     <p>{item.beschreibung}</p>
                   )}
 
-                  {item.name !== "Arkaner Fokus" && (
+                  {![
+                    "Arkaner Fokus",
+                    "Druidischer Fokus",
+                    "Geschosse",
+                    "Behälterkapazitäten",
+                    "Heiliges Symbol",
+                    "Ausrüstungspakete",
+                  ].includes(item.name) && (
                     <div className="checkbox">
                       <input type="checkbox" />
                       <label>Speichern</label>
                     </div>
                   )}
-
                 </div>
               );
             })}
