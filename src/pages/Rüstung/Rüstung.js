a6db4de477f121d0cd263964b781f13cc3044054
Eduard Metzler <eduard.metzler@mail.ru>
2023-05-09 10:18:38 +0200

    added favirite

--- src/pages/Rüstung/Rüstung.js
+++ src/pages/Rüstung/Rüstung.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { useStore } from "../../context/storeContext.js";
+import axios from "axios";
+
 
 export default function Rüstung() {
-  const { rüstung } = useStore();
+  const { rüstung, user, setUser } = useStore();
   const [filterRüstung, setFilterRüstung] = useState([]);
+
+  const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3001"
+
 
   const onChangeKlasse = (event) => {
     const value = event.target.value;
@@ -13,6 +18,26 @@ export default function Rüstung() {
     setFilterRüstung(rüstungFilter);
   };
   console.log(rüstung);
+
+  const faviriteStatus = (id, status) => {
+    console.log(status)
+    if (!status) {
+      axios
+        .post(`${BACKEND_URL}/user/new-favorite`, { id: id }, { withCredentials: true })
+
+        .then((response) => setUser(response.data.userWithoutPassword))
+        .catch((err) => console.log(err));
+    } else {
+      console.log(id)
+      axios
+        .post(`${BACKEND_URL}/user/delete-favorite`, { id: id }, { withCredentials: true })
+
+        .then((response) => setUser(response.data.userWithoutPassword))
+        .catch((err) => console.log(err));
+    }
+
+  }
+
 
   return (
     <>
@@ -40,10 +65,17 @@ export default function Rüstung() {
                   <p>Stärke: {waffe.staerke}</p>
                   <p>Rüstungsklasse: {waffe.ruestungsklasse}</p>
                   <p>Heimlichkeit: {waffe.heimlichkeit}</p>
-                  <div className="checkbox">
+                  {/* <div onClick={() => faviriteStatus(waffe._id)} className={isFavorite(waffe._id)}>
                     <input type="checkbox" />
                     <label>Speichern</label>
-                  </div>
+                  </div> */}
+                  {user.data.includes(waffe._id) ? <div onClick={() => faviriteStatus(waffe._id, true)} className="favorite">
+
+                    <label>Löschen</label>
+                  </div> : <div onClick={() => faviriteStatus(waffe._id, false)} className="noFavorite">
+
+                    <label>Speichern</label>
+                  </div>}
                 </div>
               );
             })}
